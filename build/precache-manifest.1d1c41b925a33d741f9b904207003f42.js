665282ff91dab0dffda603183f3998a021ca9a67
trevsm <trevorsm@pm.me>
2021-02-28 12:29:45 -0700

    Added build, changed .gitignore

---
+++ build/precache-manifest.1d1c41b925a33d741f9b904207003f42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a476d51ea57943d3f26855e6bedebc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aac9bf01e42097a7cb58",
+    "url": "/static/css/main.057ad2e3.chunk.css"
+  },
+  {
+    "revision": "0394bd0d513aac06caa5",
+    "url": "/static/js/2.94c1fe4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.94c1fe4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aac9bf01e42097a7cb58",
+    "url": "/static/js/main.06cd81da.chunk.js"
+  },
+  {
+    "revision": "da6844d8a89e5a2f44b0",
+    "url": "/static/js/runtime-main.ac4b94d8.js"
+  }
+]);
